4d4b58afcf15afd9a5782929bc1fcefb52a20c59
Maleyka Heybatova <6nisan2005@gmail.com>
2026-05-04 09:34:21 +0400

    add OpenCollection helper function

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -38,3 +38,8 @@ func DBinstance() *mongo.Client{
 }
 
 var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection{
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	return collection
+}
