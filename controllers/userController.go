b2cf47becc3084780fc541282ac836d2602559cb
Maleyka Heybatova <6nisan2005@gmail.com>
2026-05-04 09:47:48 +0400

    scaffold user controller with function stubs

---
+++ controllers/userController.go
@@ -0,0 +1,33 @@
+package controllers
+
+import(
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"net/http"
+	"time"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"golang-jwt-project/models"
+	helper "golang-jwt-project/helpers"
+	"golang-jwt-project/database"
+	"golang.org/x/crypto/bcrypt"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
+
+var validate = validator.New()
+
+func HashPassword()
+
+func VerifyPassword()
+
+func Signup()
+
+func Login()
+
+func GetUsers()
+
+func GetUser()
