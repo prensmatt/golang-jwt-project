d73c3bea639a6511dab7defcf01561ba39e3328e
Maleyka Heybatova <6nisan2005@gmail.com>
2026-05-11 20:03:35 +0400

    add Login handler with email lookup and password verification

--- controllers/userController.go
+++ controllers/userController.go
@@ -76,7 +76,34 @@ func Signup() gin.HandlerFunc{
 	}
 }
 
-func Login()
+func Login() gin.HandlerFunc{
+	return func(c *gin.Context){
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+
+		var foundUser models.User
+
+		if err := c.BindJSON(&user); err != nil{
+			c.JSON(http.StatusBadRequest, gin.H{"error":err.Error()})
+			return
+		}
+
+		err := userCollection.FindOne(ctx, bson.M{"email":user.Email}).Decode(&foundUser)
+
+		if err != nil{
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"email or password is incorrect"})
+			return
+		}
+
+		passwordIsValid, msg := VerifyPassword(*user.Password, *foundUser.Password)
+		if !passwordIsValid {
+    c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+    return
+
+	}
+}
 
 func GetUsers()
 
